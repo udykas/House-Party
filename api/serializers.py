4ac367a26b9c927e09f5d4172bed63bfc1e1323b
udykas <udykas@gmail.com>
2021-01-18 12:32:54 -0500

    Added a serializer, updated urls and added a Room object

---
+++ api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Room
+
+# translates python models into JSON
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guest_can_pause',
+                  'votes_to_skip', 'created_at')
