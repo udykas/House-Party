2648666c3925a455f8f169473e5fb03d0865872f
udykas <udykas@gmail.com>
2021-01-22 17:30:30 -0500

    Join Room is now fully functional

--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -19,7 +19,25 @@ class RoomJoinPage extends Component {
     }
 
     handleSubmit = () => {
-        console.log(this.state.roomCode)
+        const requestOptions={
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                code: this.state.roomCode
+            })
+        };
+
+        fetch('/api/join-room', requestOptions)
+            .then((response) => {
+                if (response.ok) {
+                    this.props.history.push(`/room/${this.state.roomCode}`)
+                } else {
+                    this.setState({ error: "Room not found" })
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+            })
     }
 
     render() {
