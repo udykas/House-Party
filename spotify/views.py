305c1d776a699b2ff2995f8014f2b7b93c38d355
udykas <udykas@gmail.com>
2021-01-27 21:13:05 -0500

    Added urls and one more endpoint for auth

--- spotify/views.py
+++ spotify/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
-from .util import update_or_create_user_tokens
+from .util import update_or_create_user_tokens, is_spotify_auth
 
 load_dotenv()
 
@@ -56,3 +56,9 @@ def spotify_callback(request, format=None):
         request.session.session_key, access_token, token_type, expires_in, refresh_token)
 
     return redirect('frontend:')
+
+
+class IsAuthenticated(APIView):
+    def get(self, request, format=None):
+        is_authenticated = is_spotify_auth(self.request.session.session_key)
+        return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
