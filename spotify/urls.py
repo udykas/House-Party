f3af622026c2177f3865f463ced1670b4352462f
udykas <udykas@gmail.com>
2021-02-24 18:43:18 -0500

    Added endpoints to play and pause Spotify

--- spotify/urls.py
+++ spotify/urls.py
@@ -3,7 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('get-auth-url', AuthURL.as_view()),
+    path('redirect', spotify_callback),
     path('is-authenticated', IsAuthenticated.as_view()),
     path('current-song', CurrentSong.as_view()),
-    path('redirect', spotify_callback),
+    path('pause', PauseSong.as_view()),
+    path('play', PlaySong.as_view()),
 ]
