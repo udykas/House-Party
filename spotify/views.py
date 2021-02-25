a3e02ae28887a364c548a04d26d65e883a523b62
udykas <udykas@gmail.com>
2021-02-24 19:29:18 -0500

    Guests can now vote to skip song

--- spotify/views.py
+++ spotify/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from .util import *
 from api.models import Room
+from .models import Vote
 
 load_dotenv()
 
@@ -106,7 +107,17 @@ class CurrentSong(APIView):
             'id': song_id
         }
 
+        self.update_room_song(room, song_id)
+
         return Response(song, status=status.HTTP_200_OK)
+
+    def update_room_song(self, room, song_id):
+        current_song = room.current_song
+
+        if current_song != song_id:
+            room.current_song = song_id
+            room.save(update_fields=['current_song'])
+            votes = Vote.objects.filter(room=room).delete()
 
 
 class PauseSong(APIView):
@@ -139,10 +150,15 @@ class SkipSong(APIView):
 
         # TODO: Check if room exists first
         room = Room.objects.filter(code=room_code)[0]
+        votes = Vote.objects.filter(room=room, song_id=room.current_song)
+        votes_needed = room.votes_to_skip
 
-        if self.request.session.session_key == room.host:
+        if self.request.session.session_key == room.host or len(votes) + 1 >= votes_needed:
+            votes.delete()
             skip_song(room.host)
         else:
-            pass
+            vote = Vote(user=self.request.session.session_key,
+                        room=room, song_id=room.current_song)
+            vote.save()
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
